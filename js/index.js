9abd672b3a0e14bf55236216d38b40b898351260
SYSTEX\1900873s <quinnwu@systex.com>
2024-03-27 15:38:05 +0800

    add assets img and test transform

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-
-
 (function () {
-    
+    window.addEventListener('orientationchange', function (e) {
+        console.log(this.document.body.clientWidth, e.target.screen.width);
+        this.document.body.width = e.target.screen.width;
+        console.log(this.document.body.width, this.document.body.clientWidth);
+    });
 })();
