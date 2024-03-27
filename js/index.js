2a4cce89eee8292c7647a90dbf78ecca752e6fff
SYSTEX\1900873s <quinnwu@systex.com>
2024-03-27 16:47:35 +0800

    test transform

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 (function () {
-    window.addEventListener('orientationchange', function (e) {
-        this.document.querySelector('.img-1').style.width = e.target.screen.width + 'px';
-        console.log(this.document.querySelector('.img-1').style.width);
+    window.addEventListener('orientationchange', function () {
+        this.document.querySelector('.img-1').style.transform =
+            'translateX(' + -1 * this.document.querySelector('.img-1').clientWidth + 'px)';
+        this.document.querySelector('.img-2').style.transform =
+            'translateX(' + -2 * this.document.querySelector('.img-2').clientWidth + 'px)';
+        this.document.querySelector('.img-3').style.transform =
+            'translateX(' + -3 * this.document.querySelector('.img-3').clientWidth + 'px)';
     });
 })();
