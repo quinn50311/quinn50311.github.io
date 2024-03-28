4ce7d66875953602d7ffba9bc457febcec105297
SYSTEX\1900873s <quinnwu@systex.com>
2024-03-28 15:40:46 +0800

    段落文字使用假文

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 (function () {
+    // 為了讓@keyframes的動畫重新渲染
     window.addEventListener('orientationchange', function () {
         this.document.styleSheets[1].cssRules[1].name = 'slider1';
-
         this.document.styleSheets[1].cssRules[1].name = 'slider';
-        console.dir(this.document.styleSheets[1].cssRules[1]);
     });
 })();
