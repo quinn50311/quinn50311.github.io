b9d957f8d7933431e5aae48aa468a8c6ea89e204
SYSTEX\1900873s <quinnwu@systex.com>
2024-03-28 10:37:18 +0800

    test modify cssRules name to reload keyframes

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 (function () {
     window.addEventListener('orientationchange', function () {
-        this.document.querySelector('.img-1').style.transform =
-            'translateX(' + -1 * this.document.querySelector('.img-1').clientWidth + 'px)';
-        this.document.querySelector('.img-2').style.transform =
-            'translateX(' + -2 * this.document.querySelector('.img-2').clientWidth + 'px)';
-        this.document.querySelector('.img-3').style.transform =
-            'translateX(' + -3 * this.document.querySelector('.img-3').clientWidth + 'px)';
+        this.document.styleSheets[1].cssRules[1].name = 'slider1';
+
+        this.document.styleSheets[1].cssRules[1].name = 'slider';
+        console.dir(this.document.styleSheets[1].cssRules[1]);
     });
 })();
